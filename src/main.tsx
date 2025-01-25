47fc861fee2cadf6793d486ef2d0c53e9ffecab0
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-25 18:46:12 +0330

    Completed Fetch API and Added React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./context/theme-provider";
 import { StrictMode } from "react";
@@ -7,10 +9,15 @@ import router from "./routers/index";
 
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
---
+++ src/services/weather.ts
@@ -0,0 +1,45 @@
+import { Coordinates, ForcastData, GeocordingResponse, WeatherData } from "@/types";
+import { API_CONFIG } from "./configApi";
+
+class WeatherAPI {
+  private createUrl(endpoint: string, params: Record<string, string | number>) {
+    const searchParams = new URLSearchParams({ appid: API_CONFIG.API_KEY, ...params })
+    return `${endpoint}?${searchParams.toString()}`
+  }
+
+  private async fetchData<T>(url: string): Promise<T> {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`Weather API Error:${response.statusText}`)
+    }
+    return response.json()
+  }
+  async getCurrentWeather({ lat, lon }: Coordinates): Promise<WeatherData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
+      lat: lat.toString(),
+      lot: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units
+    })
+    return this.fetchData<WeatherData>(url)
+  }
+  async getForeCast({ lat, lon }: Coordinates): Promise<ForcastData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/forcast`, {
+      lat: lat.toString(),
+      lot: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units
+    })
+    return this.fetchData<ForcastData>(url)
+  }
+
+  async reverseGeocode({ lat, lon }: Coordinates): Promise<GeocordingResponse[]> {
+    const url = this.createUrl(`${API_CONFIG.GEO}/reverse`, {
+      lat: lat.toString(),
+      lot: lon.toString(),
+      limit: 1,
+    })
+    return this.fetchData<GeocordingResponse[]>(url)
+  }
+
+}
+
+export const weatherApi = new WeatherAPI()
