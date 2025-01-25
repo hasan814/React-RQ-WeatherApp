c1e774d89a1ab6b45834b91cf4b3daa786efb532
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-25 15:03:25 +0330

    Added Dark Mode

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="border-t backdrop-blur">
+    <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4 text-center text-gray-400">
         <p>Footer</p>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "./context/theme-provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
@@ -8,6 +9,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
