235a2dc70c86e8417d30dc4e2bb0b406a6309326
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-14 09:22:39 +0330

    Finished Project

--- src/components/templates/NotFoundPage.tsx
+++ src/components/templates/NotFoundPage.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-6">
-      <h1 className="text-6xl font-bold text-gray-800">404</h1>
-      <p className="text-xl text-gray-600 mt-4">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-6">
+      <h1 className="text-6xl font-bold text-gray-300">404</h1>
+      <p className="text-xl text-gray-400 mt-4">
         Oops! The page you are looking for does not exist.
       </p>
       <img
